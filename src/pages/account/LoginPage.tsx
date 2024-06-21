b5579c9ed44451b68becd906152c0fb35d597aba
alessandrofoglia07 <alessandrofoglia07@gmail.com>
2024-06-21 23:29:56 +0200

    Add LoginPage

--- src/pages/account/LoginPage.tsx
+++ src/pages/account/LoginPage.tsx
@@ -1,7 +1,46 @@
-import React from 'react';
+import React, { useState } from 'react';
+
+interface Form {
+    email: string;
+    password: string;
+}
 
 const LoginPage: React.FC = () => {
-    return <div>LoginPage</div>;
+    const [formData, setFormData] = useState<Form>({
+        email: '',
+        password: ''
+    });
+
+    const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+        setFormData((prev) => ({ ...prev, [e.target.name]: e.target.value }));
+    };
+
+    const handleSubmit = (e: React.FormEvent<HTMLFormElement> | React.MouseEvent<HTMLButtonElement>) => {
+        e.preventDefault();
+    };
+
+    return (
+        <div className='bg-gradient h-svh w-full'>
+            <div className='centered bg-gradient-gray flex h-max w-[30rem] max-w-full flex-col items-center rounded-llg px-8 py-16 shadow-xl md:px-12'>
+                <h1 className='w-full text-4xl font-bold tracking-tight'>Login</h1>
+                <h2 className='w-full pt-2 tracking-wide opacity-90'>Welcome back to QuillStream.</h2>
+                <form
+                    onSubmit={handleSubmit}
+                    className='flex w-full flex-col space-y-4 pb-4 pt-12 *:rounded-llg *:border *:border-gray-600/50 *:bg-light-grey *:px-6 *:py-3 *:focus-within:outline-none'
+                    autoComplete='off'
+                    spellCheck={false}>
+                    <input type='email' name='email' placeholder='Email' value={formData.email} onChange={handleChange} />
+                    <input type='password' name='password' placeholder='Password' value={formData.password} onChange={handleChange} />
+                </form>
+                <button
+                    disabled={Object.values(formData).some((field) => field.length === 0)}
+                    className='rounded-llg border border-gray-600/50 bg-light-grey/70 px-16 py-3 transition-colors duration-75 disabled:bg-light-grey disabled:text-white/60'
+                    onClick={handleSubmit}>
+                    Login
+                </button>
+            </div>
+        </div>
+    );
 };
 
 export default LoginPage;
--- src/pages/account/RegisterPage.tsx
+++ src/pages/account/RegisterPage.tsx
@@ -27,7 +27,7 @@ const RegisterPage: React.FC = () => {
         email: '',
         password: ''
     });
-    const [error, setError] = useState<string>('');
+    const [error, setError] = useState<string | null>(null);
 
     const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
         setFormData((prev) => ({ ...prev, [e.target.name]: e.target.value }));
@@ -38,7 +38,7 @@ const RegisterPage: React.FC = () => {
 
         try {
             formSchemaSubmit.parse(formData);
-            setError('');
+            setError(null);
         } catch (err) {
             if (err instanceof ZodError) {
                 setError(err.errors[0]?.message || 'An error occurred. Please try again.');
@@ -48,22 +48,23 @@ const RegisterPage: React.FC = () => {
 
     return (
         <div className='bg-gradient h-svh w-full'>
-            <div className='centered bg-gradient-gray flex min-h-max min-w-[30rem] flex-col items-center rounded-llg px-12 py-16 shadow-xl'>
+            <div className='centered bg-gradient-gray flex h-max w-[30rem] max-w-full flex-col items-center rounded-llg px-8 py-16 shadow-xl md:px-12'>
                 <h1 className='w-full text-4xl font-bold tracking-tight'>Register</h1>
                 <h2 className='w-full pt-2 tracking-wide opacity-90'>QuillStream accounts are private and secure.</h2>
                 <form
                     onSubmit={handleSubmit}
-                    className='flex w-full flex-col space-y-4 pb-4 pt-12 *:rounded-llg *:border *:border-gray-600/50 *:bg-light-grey *:px-6 *:py-3 *:focus-within:outline-none'
+                    className='flex w-full flex-col space-y-4 pb-2 pt-12 *:rounded-llg *:border *:border-gray-600/50 *:bg-light-grey *:px-6 *:py-3 *:focus-within:outline-none'
                     autoComplete='off'
                     spellCheck={false}>
                     <input type='text' name='name' placeholder='Name' value={formData.name} onChange={handleChange} />
                     <input type='email' name='email' placeholder='Email' value={formData.email} onChange={handleChange} />
                     <input type='password' name='password' placeholder='Password' value={formData.password} onChange={handleChange} />
                 </form>
-                <h6 className='h-4 pb-12 text-center text-red-400'>{error}</h6>
+                <button className='my-4 w-2/3 rounded-llg bg-transparent py-3 text-white/60 transition-colors duration-75 hover:bg-light-grey'>Forgot Password</button>
+                {error && <h6 className='h-4 py-6 text-center text-red-400'>{error}</h6>}
                 <button
                     disabled={Object.values(formData).some((field) => field.length === 0)}
-                    className='rounded-llg border border-gray-600/50 bg-light-grey/70 px-16 py-3 transition-colors duration-75 disabled:bg-light-grey disabled:text-white/60'
+                    className='mt-4 w-2/3 rounded-llg border border-gray-600/50 bg-light-grey/70 px-16 py-3 transition-colors duration-75 disabled:bg-light-grey disabled:text-white/60'
                     onClick={handleSubmit}>
                     Register
                 </button>
