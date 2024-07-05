dd8902f96c3377f14e452bf72350dceb41427326
alessandrofoglia07 <alessandrofoglia07@gmail.com>
2024-07-05 20:32:05 +0200

    Add axios response interceptor for unexpected unauthenticated requests

--- src/api/axios.ts
+++ src/api/axios.ts
@@ -24,4 +24,41 @@ axios.interceptors.request.use((config) => {
     return config;
 });
 
+axios.interceptors.response.use(response => response, async (err) => {
+    const config = err.config;
+    if (err.response?.status === 401 && !config.__isRetryRequest) {
+        config.__isRetryRequest = true;
+        config.__retryCount = config.__retryCount || 0;
+
+        const user = userPool.getCurrentUser();
+        if (user) {
+            try {
+                await new Promise<void>((resolve, reject) => {
+                    user.getSession((err: Error | null, session: CognitoUserSession | null) => {
+                        if (err) reject(err);
+                        else {
+                            const accessToken = session!.getAccessToken().getJwtToken();
+                            config.headers['Authorization'] = `Bearer ${accessToken}`;
+                            resolve();
+                        }
+                    });
+                });
+                return axios(config);
+            } catch (err) {
+                console.error('Failed to refresh session: ', err);
+            }
+        }
+    }
+
+    if (config.__retryCount < 3) {
+        config.__retryCount++;
+        return new Promise((resolve) => {
+            setTimeout(() => resolve(axios(config)), 1000);
+        });
+    }
+
+    userPool.getCurrentUser()?.signOut();
+    return Promise.reject(err);
+});
+
 export default axios;
