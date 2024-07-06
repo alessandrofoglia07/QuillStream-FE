f8c0112eda3efd9923003fa710224705d74872d4
alessandrofoglia07 <alessandrofoglia07@gmail.com>
2024-07-06 22:48:58 +0200

    Add options ui to DocumentPreview component

--- src/components/DocumentPreview.tsx
+++ src/components/DocumentPreview.tsx
@@ -1,6 +1,9 @@
 import React from 'react';
 import type { Document, MainPageSortOption as SortOption } from '@/types';
 import { BsThreeDotsVertical as OptionsIcon } from 'react-icons/bs';
+import { Menu, MenuButton, MenuItem, MenuItems } from '@headlessui/react';
+import { FaRegEdit as RenameIcon, FaRegTrashAlt as DeleteIcon } from 'react-icons/fa';
+import { FaArrowUpRightFromSquare as OpenIcon } from 'react-icons/fa6';
 
 interface Props {
     document: Document;
@@ -30,6 +33,12 @@ const DocumentPreview: React.FC<Props> = ({ document, sortOption }) => {
         }
     };
 
+    const handleRename = async () => {};
+
+    const handleDelete = async () => {};
+
+    const handleOpenInNewTab = () => {};
+
     return (
         <div role='button' tabIndex={0} className='flex min-h-12 w-full items-center rounded-lg text-left transition-colors duration-75 *:h-full hover:bg-white/10'>
             <div className='grid w-[10%] place-items-center'>
@@ -44,11 +53,35 @@ const DocumentPreview: React.FC<Props> = ({ document, sortOption }) => {
             <div className='line-clamp-1 flex w-[15%] items-center pl-2 text-white/70'>
                 <p>{formatDate(sortOption === 'Last accessed by me' ? document.user.lastAccessedAt : document.updatedAt)}</p>
             </div>
-            <div className='grid w-[10%] place-items-center rounded'>
-                <button className='rounded-lg p-[.35rem] hover:bg-white/20'>
-                    <OptionsIcon size={20} />
-                </button>
-            </div>
+            <Menu>
+                <MenuButton>
+                    <div className='grid w-[10%] place-items-center rounded'>
+                        <button className='rounded-lg p-[.35rem] hover:bg-white/20'>
+                            <OptionsIcon size={20} />
+                        </button>
+                    </div>
+                </MenuButton>
+                <MenuItems
+                    transition
+                    anchor='bottom start'
+                    className='origin-top-right rounded-xl border border-white/5 bg-light-grey p-2 text-sm/6 text-white transition duration-100 ease-out [--anchor-gap:var(--spacing-1)] focus:outline-none data-[closed]:scale-95 data-[closed]:opacity-0'>
+                    <MenuItem>
+                        <button onClick={() => {}} className='group flex w-full items-center gap-2 rounded-lg px-3 py-1.5 data-[focus]:bg-white/10'>
+                            <RenameIcon size={20} /> Rename
+                        </button>
+                    </MenuItem>
+                    <MenuItem>
+                        <button onClick={() => {}} className='group flex w-full items-center gap-2 rounded-lg px-3 py-1.5 data-[focus]:bg-white/10'>
+                            <DeleteIcon className='-ml-0.5' size={20} /> <span className='ml-0.5'>Delete</span>
+                        </button>
+                    </MenuItem>
+                    <MenuItem>
+                        <button onClick={() => {}} className='group flex w-full items-center gap-2 rounded-lg px-3 py-1.5 data-[focus]:bg-white/10'>
+                            <OpenIcon size={18} className='mr-0.5' /> Open in new tab
+                        </button>
+                    </MenuItem>
+                </MenuItems>
+            </Menu>
         </div>
     );
 };
