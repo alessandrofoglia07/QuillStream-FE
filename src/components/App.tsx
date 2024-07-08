fbc137ff44c1b9d524b6e2218d66fd97a6e68b00
alessandrofoglia07 <alessandrofoglia07@gmail.com>
2024-07-08 19:19:06 +0200

    Add PrivateRoutes component

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -7,13 +7,15 @@ import ConfirmPage from '@/pages/account/ConfirmPage';
 import ForgotPage from '@/pages/account/ForgotPage';
 import ResetPage from '@/pages/account/ResetPage';
 import DocumentPage from '@/pages/DocumentPage';
+import PrivateRoutes from './PrivateRoutes';
 
 const App: React.FC = () => {
     return (
         <Routes>
-            <Route path='/' element={<HomePage />} />
-
-            <Route path='/documents/:documentId' element={<DocumentPage />} />
+            <Route element={<PrivateRoutes />}>
+                <Route path='/' element={<HomePage />} />
+                <Route path='/documents/:documentId' element={<DocumentPage />} />
+            </Route>
 
             <Route path='/account/create-an-account' element={<CreateAnAccountPage />} />
             <Route path='/account/confirm' element={<ConfirmPage />} />
